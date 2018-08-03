74eaa5cf290fb6b4bd657403e21fe7f7f30a6744
Carl Winkler <carl@winkler.id.au>
2018-08-03 12:05:44 +0800

    Do rolling backups

--- sync.sh
+++ sync.sh
@@ -6,6 +6,8 @@ echo "Job started: $(date)"
 
 rm -rf /tmp/backup.tar.gz
 tar -zcf /tmp/backup.tar.gz -C $DATA_PATH .
+export TIME_PATH=$S3_PATH-$(date --iso-8601=seconds)
+/usr/local/bin/s3cmd sync $PARAMS "$S3_PATH" "$TIME_PATH" || echo "First time"
 /usr/local/bin/s3cmd sync $PARAMS /tmp/backup.tar.gz "$S3_PATH"
 
 echo "Job finished: $(date)"
